0f7a352071cb5593a5f11d1112617221048fca8d
Alvin Tseng <alvin.tseng@external.stellantis.com>
2022-08-18 16:58:57 +0800

    Initial Commit

---
+++ SwiftLearns/SwiftLearnsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearnsApp.swift
+//  SwiftLearns
+//
+//  Created by Alvin Tseng on 2022/8/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearnsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
